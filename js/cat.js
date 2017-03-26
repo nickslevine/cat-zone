ada9fc90895b1c0cfd3881a7f197f8dd275e56af
Nick Levine <nickslevine@gmail.com>
2017-03-26 11:09:16 +0100

    added pics

--- js/cat.js
+++ js/cat.js
@@ -1,7 +1,7 @@
 let p = document.getElementById("catpic");
 let pics = 1
 document.getElementById("catpic").addEventListener("click", function(){
-  if (pics == 6) {pics = 1}
+  if (pics == 7) {pics = 1}
   else {pics += 1};
   let file = `img/${pics}.jpg`
   document.getElementById("catpic").setAttribute("src", file);
